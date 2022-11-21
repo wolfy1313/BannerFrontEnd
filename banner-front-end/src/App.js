ea437b8dcc7978ecae3496106ebdfcf806547c25
jeremyHarrell <hillvulf@gmail.com>
2022-11-21 11:28:02 -0600

    created components and input Auth boilerplate

--- banner-front-end/src/App.js
+++ banner-front-end/src/App.js
@@ -1,9 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
 import './App.css';
+import Nav from './components/Nav';
+import Register from './components/Register';
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+  
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+  
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+
+
   return (
-    <div className="App"> <h1>App</h1>
+    <div className="App">
+      <Nav
+        authenticated={authenticated}
+        user={user}
+        handleLogOut={handleLogOut}
+      />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </main>
     </div>
   );
 }
