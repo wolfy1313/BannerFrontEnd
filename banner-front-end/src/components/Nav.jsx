f6b6e5f5b52e175d66fc96fa7ee57d6240a2a366
jeremyHarrell <hillvulf@gmail.com>
2022-11-21 13:44:11 -0600

    updated login route

--- banner-front-end/src/components/Nav.jsx
+++ banner-front-end/src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = ({authenticated, user, handleLogOut}) => {
   return (
     <div className={authenticated ? "nav-div" : "logged-out-nav-div"}>
       <h1>Banner</h1>
-      <h6>{authenticated ? <Link onClick={handleLogOut}to='/'>Log Out</Link> : <Link to='/'>Login</Link>} | <Link to='/register'>Register</Link> | {authenticated ? <Link to={`/user/${user.id}`}>Home</Link> : <Link to='/'>Home</Link>}</h6>
+      <h6>{authenticated ? <Link onClick={handleLogOut}to='/'>Log Out</Link> : <Link to='/login'>Login</Link>} | <Link to='/register'>Register</Link> | {authenticated ? <Link to={`/user/${user.id}`}>Home</Link> : <Link to='/'>Home</Link>}</h6>
     </div>
   )
 }
