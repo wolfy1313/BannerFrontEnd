f3343958db69906491fb1f97106a3bd0df77a8c2
jeremyHarrell <hillvulf@gmail.com>
2022-11-21 13:00:17 -0600

    created Nav functionality

--- banner-front-end/src/components/Nav.jsx
+++ banner-front-end/src/components/Nav.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Nav = ({authenticated, user,handleLogOut}) => {
+const Nav = ({authenticated, user, handleLogOut}) => {
+
   return (
-    <div>Nav</div>
+    <div className={authenticated ? "nav-div" : "logged-out-nav-div"}>
+      <h1>Banner</h1>
+      <h6>{authenticated ? <Link onClick={handleLogOut}to='/'>Log Out</Link> : <Link to='/'>Login</Link>} | <Link to='/register'>Register</Link> | {authenticated ? <Link to={`/user/${user.id}`}>Home</Link> : <Link to='/'>Home</Link>}</h6>
+    </div>
   )
 }
 
