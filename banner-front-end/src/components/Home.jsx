4d0f09aa8be6b995e62ebe47fe8a46c8925e59e0
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-11-21 13:03:32 -0600

    prepare for pull

--- banner-front-end/src/components/Home.jsx
+++ banner-front-end/src/components/Home.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
+
+  const [classes, setClasses] = useState([])
+
+//gets all classes
+
+useEffect(()=>{
+  const apiCall = async ()=>{
+    let response = await axios.get(`http://localhost:3001/classes`)
+    setClasses(response.data)
+  }
+  apiCall()
+},[])
+
+
+  
   return (
-    <div>Home</div>
+    <div>
+      <h1>Current Courses</h1>
+      <section>
+      {classes.map((class) => (
+            <div >
+              <h2>{class.name}</h2>
+            </div>
+          ))}
+      </section>
+      </div>
   )
 }
 
